e692152f2db7b010e3d1fa30d325f3790856e5c2
Ashraf Hussain <siddiquiashrafhussain@users.noreply.github.com>
2026-08-12 22:20:02 +0530

    Add solution: Length of Longest Subarray With at Most K Frequency

---
+++ Length of Longest Subarray With at Most K Frequency.java
@@ -0,0 +1,103 @@
+// 2958. Length of Longest Subarray With at Most K Frequency
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer array nums and an integer k.
+
+// The frequency of an element x is the number of times it occurs in an array.
+
+// An array is called good if the frequency of each element in this array is less than or equal to k.
+
+// Return the length of the longest good subarray of nums.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,2,3,1,2], k = 2
+// Output: 6
+// Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
+// It can be shown that there are no good subarrays with length more than 6.
+// Example 2:
+
+// Input: nums = [1,2,1,2,1,2,1,2], k = 1
+// Output: 2
+// Explanation: The longest possible good subarray is [1,2] since the values 1 and 2 occur at most once in this subarray. Note that the subarray [2,1] is also good.
+// It can be shown that there are no good subarrays with length more than 2.
+// Example 3:
+
+// Input: nums = [5,5,5,5,5,5,5], k = 4
+// Output: 4
+// Explanation: The longest possible good subarray is [5,5,5,5] since the value 5 occurs 4 times in this subarray.
+// It can be shown that there are no good subarrays with length more than 4.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 109
+// 1 <= k <= nums.length
+
+
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int n = nums.length;
+
+        int size = 1;
+        while (size < n * 4) size <<= 1;
+
+        int[] keys = new int[size];
+        int[] vals = new int[size];
+        boolean[] used = new boolean[size];
+        int mask = size - 1;
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            int x = nums[right];
+            int idx = hash(x) & mask;
+
+            while (used[idx] && keys[idx] != x) {
+                idx = (idx + 1) & mask;
+            }
+
+            if (!used[idx]) {
+                used[idx] = true;
+                keys[idx] = x;
+            }
+
+            vals[idx]++;
+
+            while (vals[idx] > k) {
+                int y = nums[left++];
+                int j = hash(y) & mask;
+
+                while (keys[j] != y) {
+                    j = (j + 1) & mask;
+                }
+
+                vals[j]--;
+            }
+
+            int len = right - left + 1;
+            if (len > ans) ans = len;
+        }
+
+        return ans;
+    }
+
+    private int hash(int x) {
+        x ^= x >>> 16;
+        x *= 0x7feb352d;
+        x ^= x >>> 15;
+        x *= 0x846ca68b;
+        x ^= x >>> 16;
+        return x;
+    }
+}
